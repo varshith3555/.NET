7f55cf87174bf97f58c3b2b6fe9e697478c33957
varshith3555 <varshithreddy661@gmail.com>
2025-12-18 19:08:24 +0530

    Day 1: Practise

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter name:");
+        string? name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+
+        // Call practice files directly
+        Day1Practise.Run();        
+    }
+}
+
