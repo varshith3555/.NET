665dbad122828e76fff892e5ce28b1b9212e0dcb
varshith3555 <varshithreddy661@gmail.com>
2026-01-06 21:12:10 +0530

    Serialization & Delegates

---
+++ Day10 Serialization & Delegates/Delegates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyConsoleApp.Day10
+{
+    public delegate int DelegateAddFunctionName(int a, int b);
+    public class ExampleOfDelegate
+    {
+        public int a;
+        public int b;
+
+        public void DelegateEx1()
+        {
+            DelegateAddFunctionName delegateVarable = new DelegateAddFunctionName(AddMethod3);
+            delegateVarable(1, 2);
+        }
+
+        private int AddMethod3(int a, int b)
+        {
+            return a + b + 40;
+        }
+
+        private int AddMethod2(int a, int b)
+        {
+            return a + b + 10;
+        }
+    }
+}
---
+++ Day10 Serialization & Delegates/ObjectToXML.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+// Serialization
+
+namespace MyConsoleApp.Day10
+{
+   public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int[] arr;
+        public List<int> scores { get; set; }
+        public Employee()
+        {
+            scores = new List<int> { 85, 90, 78, 92 };
+        }
+    }
+
+    class Employee2
+    {
+        static void Main()
+        {
+            Employee emp = new Employee
+            { // Create object
+                Id = 1,
+                Name = "XML",
+                arr = new int[] { 1, 2 }
+            };
+
+
+            /// We use XmlSerializer class to convert an object into XML.
+            
+            //XmlSerializer xml = new XmlSerializer(typeof(Employee));
+            //using (StringWriter  sw = new StringWriter())
+            //{
+            //    xml.Serialize(sw, emp);
+            //    string xmlStr = sw.ToString();
+            //    Console.WriteLine(xmlStr);
+            //} 
+            // or 
+            //XmlSerializer xml = new XmlSerializer(emp.GetType());
+            //xml.Serialize(Console.Out, emp);
+
+
+            // SERIALIZATION: Object → JSON
+            string jsonData = JsonSerializer.Serialize(emp);
+            Console.WriteLine(jsonData);
+            
+            Employee newemp = JsonSerializer.Deserialize<Employee>(jsonData);
+            Console.WriteLine(newemp.Id);
+            Console.WriteLine(newemp.Name);
+            Console.WriteLine("Scores: " + string.Join(",", newemp.scores));
+
+        }
+    }
+}
