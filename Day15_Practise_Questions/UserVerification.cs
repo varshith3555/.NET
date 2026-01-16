6090fd6f70acc24a8e231fc21f73b504cd91424f
varshith3555 <varshithreddy661@gmail.com>
2026-01-16 12:24:38 +0530

    new commit

---
+++ Day15_Practise_Questions/UserVerification.cs
@@ -0,0 +1,45 @@
+namespace Day15_Practise_Questons
+{
+    class InvalidPhoneNumberException : Exception
+    {
+        public InvalidPhoneNumberException(string message) : base(message)
+        {
+        }
+    }
+    class User1
+    {
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+    class UserVerification
+    {
+        public static User1 ValidatePhoneNumber(string name, string phoneNumber)
+        {
+            if (phoneNumber.Length == 10)
+            {
+                User1 user = new User1();
+                user.Name = name;
+                user.PhoneNumber = phoneNumber;
+                return user;
+            }
+            else
+            {
+                throw new InvalidPhoneNumberException("Invalid phone number");
+            }
+        }
+        static void Main()
+        {
+            try
+            {
+                string name = Console.ReadLine();
+                string phoneNumber = Console.ReadLine();
+
+                User1 res = ValidatePhoneNumber(name, phoneNumber);
+                Console.WriteLine("User verified successfully");
+            }catch (InvalidPhoneNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }   
+}
