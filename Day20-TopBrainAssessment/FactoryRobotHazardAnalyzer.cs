55485652d63e6c1e6b1ca33831da6b899d0651ed
varshith3555 <varshithreddy661@gmail.com>
2026-02-03 23:40:02 +0530

    new commit

--- Day20-TopBrainAssessment/FactoryRobotHazardAnalyzer.cs
+++ Day20-TopBrainAssessment/FactoryRobotHazardAnalyzer.cs
@@ -16,14 +16,17 @@ public class RobotHazardAuditor
         {
             throw new RobotSafetyException("Error:  Arm precision must be 0.0-1.0");
         }
+
         if(workerDensity < 1 || workerDensity > 20)
         {
             throw new RobotSafetyException("Error: Worker density must be 1-20");
         }
+
         if(machineryState != "Worn" && machineryState != "Faulty" && machineryState != "Critical")
         {
             throw new RobotSafetyException("Error: Unsupported machinery state");
         }
+        
         if(machineryState == "Worn")
             riskFactor = 1.3;
         else if(machineryState == "Faulty")
