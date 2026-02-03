748a3c53c9a3ad87c4f78ea4d2af93efe0912b3d
varshith3555 <varshithreddy661@gmail.com>
2026-02-03 23:41:58 +0530

    new commit

---
+++ Day20-TopBrainAssessment/UnitTest.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class UnitTest
+{
+    private Program bankAccount;
+
+    [SetUp]
+    public void Setup()
+    {
+        bankAccount = new Program(1000m);
+    }
+
+    [Test]
+    public void Test_Deposit_ValidAmount()
+    {
+        bankAccount.Deposit(500m);
+        Assert.AreEqual(1500m, bankAccount.Balance);
+    }
+
+    [Test]
+    public void Test_Deposit_NegativeAmount()
+    {
+        Assert.Throws<System.Exception>(() => bankAccount.Deposit(-100m));
+    }
+
+    [Test]
+    public void Test_Withdraw_ValidAmount()
+    {
+        bankAccount.Withdraw(300m);
+        Assert.AreEqual(700m, bankAccount.Balance);
+    }
+
+    [Test]
+    public void Test_Withdraw_InsufficientFunds()
+    {
+        Assert.Throws<System.Exception>(() => bankAccount.Withdraw(1500m));
+    }
+}
