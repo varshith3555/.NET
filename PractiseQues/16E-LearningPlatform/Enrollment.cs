39e046dbf1176adc9b2de397bc5a6b9a189f0f26
varshith3555 <varshithreddy661@gmail.com>
2026-02-05 23:38:05 +0530

    new commit

---
+++ PractiseQues/16E-LearningPlatform/Enrollment.cs
@@ -0,0 +1,11 @@
+namespace _16ELearningPlatform
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public string StudentId { get; set; }
+        public string CourseCode { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public double ProgressPercentage { get; set; }
+    }
+}
