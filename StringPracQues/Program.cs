49b1f348f9f68192ce0df9c2fc789dae03124b2b
varshith3555 <varshithreddy661@gmail.com>
2026-02-12 11:32:26 +0530

    Updated with Largest element code

--- StringPracQues/Program.cs
+++ StringPracQues/Program.cs
@@ -4,13 +4,27 @@ class Program{
     static void Main(){
 
         #region Reverse
-        string input1 = Console.ReadLine()!;
-        string rev = "";
-        for(int i = input1.Length - 1; i >= 0; i--)
-        {
-            rev += input1[i];
-        }
-        System.Console.WriteLine(rev);
+        // string input1 = Console.ReadLine()!;
+        // string rev = "";
+        // for(int i = input1.Length - 1; i >= 0; i--)
+        // {
+        //     rev += input1[i];
+        // }
+        // System.Console.WriteLine(rev);
         #endregion
+
+        #region largest element
+        int[] arr = {1,3,7,9};
+        int max = arr[0];
+        foreach(var i in arr)
+        {
+            if(i > max)
+            {
+                max = i;
+            }
+        }
+        System.Console.WriteLine(max);
+        #endregion
+
     }
 }
