e33a4561e8a3a34ec03190a064f6c60e3a176e1b
varshith3555 <varshithreddy661@gmail.com>
2026-02-12 11:30:50 +0530

    String Reverse

---
+++ StringPracQues/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+
+class Program{
+    static void Main(){
+
+        #region Reverse
+        string input1 = Console.ReadLine()!;
+        string rev = "";
+        for(int i = input1.Length - 1; i >= 0; i--)
+        {
+            rev += input1[i];
+        }
+        System.Console.WriteLine(rev);
+        #endregion
+    }
+}
