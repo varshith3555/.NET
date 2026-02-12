0057336e4a0d0cb8749b640108a499d1919f0c6c
varshith3555 <varshithreddy661@gmail.com>
2026-02-12 11:34:54 +0530

    Updated file with Remove duplicates code

--- StringPracQues/Program.cs
+++ StringPracQues/Program.cs
@@ -14,17 +14,36 @@ class Program{
         #endregion
 
         #region largest element
-        int[] arr = {1,3,7,9};
-        int max = arr[0];
-        foreach(var i in arr)
-        {
-            if(i > max)
-            {
-                max = i;
-            }
-        }
-        System.Console.WriteLine(max);
+        // int[] arr = {1,3,7,9};
+        // int max = arr[0];
+        // foreach(var i in arr)
+        // {
+        //     if(i > max)
+        //     {
+        //         max = i;
+        //     }
+        // }
+        // System.Console.WriteLine(max);
         #endregion
 
+        #region Remove duplicates
+
+        System.Console.WriteLine("Enter the no of elements");
+        int n = int.Parse(Console.ReadLine()!);
+        
+        List<int> list = new List<int>(); 
+        System.Console.WriteLine("Enter the elements");
+        for(int i = 0; i < n; i++)
+        {
+            int temp = int.Parse(Console.ReadLine()!);
+            list.Add(temp);
+        }
+        
+        HashSet<int> uniqueNumbers = new HashSet<int>(list);
+        foreach(int num in uniqueNumbers)
+        {
+            Console.Write(num + " ");
+        }
+        #endregion
     }
 }
