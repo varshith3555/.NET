3e9e8fbb82f9f75f6c24019868a6a6d2088c1060
varshith3555 <varshithreddy661@gmail.com>
2026-02-12 18:29:48 +0530

    Meeting Questions

--- Day22-UseCasePractice/GymStream/Program.cs
+++ Day22-UseCasePractice/GymStream/Program.cs
@@ -1,38 +1,27 @@
 ﻿namespace GymStream{
-
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Membership member = new Membership();
+            string s = "Varshith";
+            // string rev = "";
 
-            // TODO: Wrap the following in a Try-Catch block
-            try{
-                Console.WriteLine("Enter membership tier:");
-                member.Tier = Console.ReadLine();
+            // for(int i = s.Length - 1; i >= 0; i--){
+            //     rev += s[i];
+            // }
+            // Console.WriteLine(rev);
 
-                Console.WriteLine("Enter duration:");
-                member.DurationInMonths = int.Parse(Console.ReadLine()!);
 
-                Console.WriteLine("Enter monthly price:");
-                member.BasePricePerMonth = double.Parse(Console.ReadLine()!);
-
-                // TODO: Call validation and display result/price
-                if (member.ValidateEnrollment())
-                {
-                    double bill = member.CalculateTotalBill();
-                    System.Console.WriteLine("\nEnrollment Successful!");
-                    System.Console.WriteLine($"Total Bill Amount: ₹{bill:F2}");
-                }
-            }
-            catch(InvalidTierException e){
-                System.Console.WriteLine(e.Message);
-            }
-            catch(Exception ex)
+            for(int i = 0; i < s.Length; i++)
             {
-                System.Console.WriteLine(ex.Message);
+                int count = 0;
+                for(int j = 0; j < s.Length; j++)
+                {
+                    if(s[i] == s[j])
+                        count++;
+                }
+                System.Console.WriteLine(count);
             }
         }
     }
-
 }
