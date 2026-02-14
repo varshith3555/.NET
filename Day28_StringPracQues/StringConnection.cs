a0380b71a18adabfb1daa8c55cb444f3fcaacb8a
varshith3555 <varshithreddy661@gmail.com>
2026-02-14 23:36:31 +0530

    Updated the folder name with Day

---
+++ Day28_StringPracQues/StringConnection.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+
+//using System.Data.SqlClient;
+//namespace MyConsoleApp.StringPracQues
+//{
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        string connectionString =
+//        "Server=REDDV\\SQLEXPRESS;Database=lpuDB;Trusted_Connection=True;";
+
+//        using (SqlConnection conn = new SqlConnection(connectionString))
+//        {
+//            try
+//            {
+//                conn.Open();
+//                Console.WriteLine("Connection Successful!");
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("Error: " + ex.Message);
+//            }
+//        }
+//    }
+//}
+
+//}
