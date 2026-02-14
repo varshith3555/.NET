e9342498e7f30f0e75ab700f4a586586090ef5f6
varshith3555 <varshithreddy661@gmail.com>
2026-02-14 23:32:36 +0530

    Updated the folder name

---
+++ Day23-UseCasePractice/LogisticsPro/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LogisticsPro
+{
+    class Program
+    {
+        static void Main()
+        {
+            ShipmentDetails sd = new ShipmentDetails();
+            System.Console.WriteLine("Enter the Shipment code");
+            sd.ShipmentCode = Console.ReadLine()!;
+
+            if (sd.ValidateShipmentCode())
+            {
+                System.Console.WriteLine("Enter the transmode mode");
+                sd.TransportMode = Console.ReadLine()!;
+                System.Console.WriteLine("Enter weight");
+                sd.Weight = double.Parse(Console.ReadLine()!);
+                System.Console.WriteLine("Enter storage days");
+                sd.StorageDays = int.Parse(Console.ReadLine()!);
+                
+                double cost = sd.CalculateTotalCost();
+                System.Console.WriteLine("The total shipping cost is" + cost);
+            }else{
+                System.Console.WriteLine("Invalid shipment code");
+            }
+        }
+    }
+}
