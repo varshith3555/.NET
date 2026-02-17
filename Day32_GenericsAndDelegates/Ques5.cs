fe704b38aca764c34085dbf8100bbbf9530bbbcb
varshith3555 <varshithreddy661@gmail.com>
2026-02-17 16:10:29 +0530

    Predicate Filter

---
+++ Day32_GenericsAndDelegates/Ques5.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+public class Program5
+{
+    public static void Main()
+    {
+        var nums = new List<int> { 2, 5, 8, 11, 14 };
+
+        var evens = Filter(nums, n => n % 2 == 0);
+        Console.WriteLine(string.Join(",", evens));         // Expected: 2,8,14
+
+        var big = Filter(nums, n => n >= 10);
+        Console.WriteLine(string.Join(",", big));           // Expected: 11,14
+    }
+
+    // ✅ TODO: Students implement only this function
+    public static List<T> Filter<T>(List<T> items, Predicate<T> match)
+    {
+        // TODO: return a new list with matched items
+        var res = new List<T>();
+        foreach(var i in items)
+        {
+            if (match(i))
+            {
+                res.Add(i);
+            }
+        }
+        return res;
+    }
+}
